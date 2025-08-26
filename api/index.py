83e3360f310fd5473a68b45cfdcfa077c8bb5662
Elvin Cheung <111124105+elvincheung0321@users.noreply.github.com>
2025-08-26 06:16:20 +0000

    name

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nba.settings')  # Replace 'nba' with your project name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nba.settings') 
 app = get_wsgi_application()
